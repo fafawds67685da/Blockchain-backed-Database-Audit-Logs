5024c22bdcec684d14d0849845091a572f837ce8
fafawds67685da <gdev6886@gmail.com>
2025-10-04 11:09:04 +0530

    Database created

---
+++ Database/employees.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (name, role, salary) VALUES ('Yash Sahni', 'Member 1', 50000);
+INSERT INTO employees (name, role, salary) VALUES ('Alice Kumar', 'Member 2', 60000);
---
+++ export_live_csv.py
@@ -0,0 +1,26 @@
+import psycopg2
+import pandas as pd
+import time
+
+# PostgreSQL connection
+conn = psycopg2.connect(
+    dbname="audit_logs",
+    user="postgres",
+    password="devsidhu@59",
+    host="localhost",
+    port="5432"
+)
+
+csv_file = "D:/Blockchain-backed-Database-Audit-Logs/Database/employees_live.csv"
+
+while True:
+    # Fetch data from employees table
+    df = pd.read_sql_query("SELECT * FROM employees", conn)
+    
+    # Write to CSV (overwrite each time)
+    df.to_csv(csv_file, index=False)
+    
+    print(f"CSV updated! {len(df)} rows saved.")
+    
+    # Wait before next update (e.g., 10 seconds)
+    time.sleep(10)
